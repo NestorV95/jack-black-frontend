3b88631c083fa1ee1d9e3b18141eeedc6e354f1a
hadi <hadi_960@hotmail.com>
2021-04-21 13:24:54 -0500

    fixes validations

--- index.js
+++ index.js
@@ -2,14 +2,9 @@ document.addEventListener('DOMContentLoaded',e=>{
     // fetchGames()
     // fetchUsers()
     // fetchDealers()
-<<<<<<< HEAD
-    // showSignUpForm()
-       showUserHandEditForm()
-    //    showEditForm()
-=======
     //showSignUpForm()
     //showUserEditForm()
     //showDeleteButton()
     //fetchDealerHands()
->>>>>>> bded02f058ceeb470f2eedf9acefefdcf789ea5b
 })
+
